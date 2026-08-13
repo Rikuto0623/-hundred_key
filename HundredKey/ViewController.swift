34f5b7c255b546e0d1d6418652f5114eed9f8be9
Rikuto0623 <rikuriku0623to@gmail.com>
2026-08-13 16:37:42 +0900

    Add files via upload

---
+++ HundredKey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HundredKey
+//
+//  Created by 鈴木久美 on 2026/08/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
